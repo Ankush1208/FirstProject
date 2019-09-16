68271f946a5a8e00a5a81c49caabcb6f2faf701b
Your Name <ankushvashishth12@gmail.com>
2019-09-16 14:23:11 +0530

    changed

--- src/main/java/com/notification/constants/ApiConstants.java
+++ src/main/java/com/notification/constants/ApiConstants.java
@@ -2,6 +2,6 @@ package com.notification.constants;
 
 public interface ApiConstants {
 
-	public static final String ACCOUNT_SID = "ACf768cce11f01412e249b0280a7c502ec";
-    public static final String AUTH_TOKEN = "092ce1357aa5bb48a1aa24e6a9e68452";
+	public static final String ACCOUNT_SID = "";
+    public static final String AUTH_TOKEN = "";
 }
